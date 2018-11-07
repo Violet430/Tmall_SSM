498c6d70e88a3bf0aac3a7a64681683d6b8fc1fc
polarbear430 <1525625981@qq.com>
2018-11-07 16:35:38 +0800

    首页 注册功能

--- src/main/java/com/viol3t/tmall/service/ProductService.java
+++ src/main/java/com/viol3t/tmall/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.viol3t.tmall.service;
 
+import com.viol3t.tmall.pojo.Category;
 import com.viol3t.tmall.pojo.Product;
 
 import java.util.List;
@@ -12,4 +13,8 @@ public interface ProductService {
     List list(int cid);
     //新增--》产品图片
     void setFirstProductImage(Product p);
+
+    void fill(List<Category> cs);
+    void fill(Category c);
+    void fillByRow(List<Category> cs);
 }
